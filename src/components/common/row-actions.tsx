53442c13353d3f81bfdbb45b4219f0afc8ce70fe
Waleed Malik <ahmedwaleedmalik@gmail.com>
2026-03-12 16:26:07 +0500

    feat: add YAML viewer, delete dialog, row actions reusable CRUD components

---
+++ src/components/common/row-actions.tsx
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2026 The KubeLB Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ComponentType } from "react";
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+interface RowAction {
+  label: string;
+  icon?: ComponentType<{ className?: string }>;
+  onClick: () => void;
+  variant?: "default" | "destructive";
+  separator?: boolean;
+}
+
+interface RowActionsProps {
+  actions: RowAction[];
+}
+
+export function RowActions({ actions }: RowActionsProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        render={
+          <Button variant="ghost" size="sm" onClick={(e) => e.stopPropagation()}>
+            <MoreHorizontal data-icon />
+            <span className="sr-only">Open actions</span>
+          </Button>
+        }
+      />
+      <DropdownMenuContent align="end">
+        <DropdownMenuGroup>
+          {actions.map((action) => (
+            <span key={action.label}>
+              {action.separator && <DropdownMenuSeparator />}
+              <DropdownMenuItem variant={action.variant} onClick={action.onClick}>
+                {action.icon && <action.icon data-icon />}
+                {action.label}
+              </DropdownMenuItem>
+            </span>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export type { RowAction, RowActionsProps };
